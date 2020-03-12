7aab6b52494e39783c9f5da844d1a6a020bf6905
Christian Williams <christianw@bushelpowered.com>
2020-03-12 00:49:58 -0700

    WIP maybe working

--- integrations/gradle-plugin/src/main/groovy/org/robolectric/gradleplugin/GradlePlugin.groovy
+++ integrations/gradle-plugin/src/main/groovy/org/robolectric/gradleplugin/GradlePlugin.groovy
@@ -11,12 +11,24 @@ import org.gradle.api.tasks.testing.Test
 class GradlePlugin implements Plugin<Project> {
     public static final String downloadTaskName = "robolectricDownloadAndroidSdks"
 
+    static class Config {
+        Object sdks
+    }
+
     @Override
     public void apply(Project project) {
-        def selfTestClassPath = System.getenv("gradle-robolectric-plugin.classpath")
+        Config config = new Config()
+        project.extensions.add("robolectric", config)
         def downloadTask = project.getTasks().create(downloadTaskName, DownloadAndroidSdks.class)
-        def robolectricVersion = GradlePlugin.class.classLoader
-                .getResource("robolectric-version.txt").text
+
+        String robolectricVersion = loadResourceFile("robolectric-version.txt")
+        provideSdks(project, config)
+        addRobolectricDependencies(project, robolectricVersion)
+        enableIncludeAndroidResources(project, downloadTask)
+    }
+
+    private addRobolectricDependencies(Project project, robolectricVersion) {
+        def selfTestClassPath = System.getenv("gradle-robolectric-plugin.classpath")
 
         project.getGradle().addListener(new DependencyResolutionListener() {
             @Override
@@ -39,7 +51,9 @@ class GradlePlugin implements Plugin<Project> {
             public void afterResolve(ResolvableDependencies resolvableDependencies) {
             }
         })
+    }
 
+    private enableIncludeAndroidResources(Project project, downloadTask) {
         project.afterEvaluate { p ->
             Object androidExt = project.getExtensions().findByName("android")
             if (androidExt == null) {
@@ -57,5 +71,91 @@ class GradlePlugin implements Plugin<Project> {
                 }
             }
         }
+    }
+
+    void provideSdks(Project project, Config config) {
+        project.repositories.add(project.repositories.jcenter())
+
+        project.afterEvaluate {
+            def defaultSdks = loadPropertiesResourceFile("org.robolectric.GradlePlugin.sdks.properties")
+            def enabledSdks = figureSdks(config.sdks, defaultSdks)
+            Properties sdkDeps = new Properties()
+            enabledSdks.keySet().forEach { Integer apiLevel ->
+                def sdkCoords = enabledSdks.get(apiLevel)
+                if (sdkCoords instanceof File) {
+                    sdkCoords = project.files(sdkCoords)
+                }
+                def sdkCfg = project.configurations.create("sdk$apiLevel")
+                project.dependencies.add("sdk$apiLevel", sdkCoords)
+
+                def sdkFiles = sdkCfg.resolve()
+                if (sdkFiles.size() != 1) {
+                    throw new IllegalStateException("weird, $sdkCoords returned $sdkFiles, not one file")
+                }
+                sdkDeps[apiLevel.toString()] = sdkFiles[0].toString()
+                println "sdk$apiLevel = ${sdkFiles}"
+            }
+
+            def outDir = new File(project.buildDir, "generated/robolectric")
+            outDir.mkdirs()
+            project.android.sourceSets['test'].resources.srcDir(outDir)
+            def outFile = new File(outDir, 'org.robolectric.sdks.properties')
+            def out = outFile.newOutputStream()
+            sdkDeps.store(out, null)
+            out.close()
+            println "xxx wrote to $outFile"
+            println "props: $sdkDeps"
+        }
+    }
+
+    Map<Integer, String> figureSdks(Object configSdks, Properties defaultSdks) {
+        def map = new HashMap<Integer, String>()
+
+        def add = { Integer apiLevel ->
+            def coordinates = defaultSdks.getProperty(apiLevel.toString())
+            if (coordinates == null) {
+                throw new IllegalArgumentException("Unknown API level $apiLevel")
+            }
+            map.put(apiLevel, coordinates)
+        }
+
+        if (configSdks instanceof String) {
+            configSdks.split(",").each { add(Integer.parseInt(it)) }
+        } else if (configSdks instanceof Integer) {
+            add(configSdks)
+        } else if (configSdks instanceof List
+                || configSdks instanceof int[]
+                || configSdks instanceof Object[]) {
+            configSdks.iterator().forEachRemaining {
+                if (it instanceof String) add(Integer.parseInt(it)) else add((int) it)
+            }
+        } else if (configSdks instanceof Map) {
+            configSdks.keySet().forEach { key ->
+                def coord = configSdks.get(key)
+                if (coord instanceof String) {
+                    if (coord.contains("/")) coord = new File(coord)
+                }
+                map.put(key, coord)
+            }
+        }
+        return map
+    }
+
+    String loadResourceFile(String name) {
+        def resource = GradlePlugin.class.classLoader.getResource(name)
+        if (resource == null) throw new IllegalStateException("$name not found")
+        return resource.text
+    }
+
+    Properties loadPropertiesResourceFile(String name) {
+        def props = new Properties()
+        def resourceIn = GradlePlugin.class.classLoader.getResourceAsStream(name)
+        if (resourceIn == null) throw new IllegalStateException("$name not found")
+        try {
+            props.load(resourceIn)
+        } finally {
+            resourceIn.close()
+        }
+        return props
     }
 }
--- integrations/gradle-plugin/src/test/java/org/robolectric/gradleplugin/GradlePluginTest.java
+++ integrations/gradle-plugin/src/test/java/org/robolectric/gradleplugin/GradlePluginTest.java
@@ -1,34 +1,83 @@
 package org.robolectric.gradleplugin;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.DependencySet;
+import org.gradle.api.internal.project.DefaultProject;
 import org.gradle.testfixtures.ProjectBuilder;
-import org.gradle.testkit.runner.BuildResult;
-import org.gradle.testkit.runner.GradleRunner;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class GradlePluginTest {
 
   private File gradleProjectDir;
   private String androidSdkRoot;
+  private GradlePlugin plugin;
+  private Properties defaultSdks;
 
   @Before
   public void setUp() throws Exception {
-    gradleProjectDir = new File(getClass().getResource("/testProject").getFile());
-
-    androidSdkRoot = System.getenv("ANDROID_SDK_ROOT");
-    if (androidSdkRoot == null) {
-      fail("ANDROID_SDK_ROOT should've been set by gradle-plugin's build.gradle.");
-    }
+    gradleProjectDir = Helpers.findTestProject();
+    androidSdkRoot = Helpers.findAndroidSdkRoot();
+    plugin = new GradlePlugin();
+    defaultSdks = new Properties();
+    defaultSdks.setProperty("17", "sdk17.jar");
+    defaultSdks.setProperty("18", "sdk18.jar");
+    defaultSdks.setProperty("19", "sdk19.jar");
   }
 
   @Test
-  public void greeterPluginAddsGreetingTaskToProject() {
+  public void figureSdksAcceptsStrings() throws Exception {
+    assertThat(plugin.figureSdks("17", defaultSdks))
+        .isEqualTo(mapOf(17, "sdk17.jar"));
+  }
+
+  @Test
+  public void figureSdksAcceptsInts() throws Exception {
+    assertThat(plugin.figureSdks(18, defaultSdks))
+        .isEqualTo(mapOf(18, "sdk18.jar"));
+  }
+
+  @Test
+  public void figureSdksAcceptsIntArrays() throws Exception {
+    assertThat(plugin.figureSdks(new int[] {17, 19}, defaultSdks))
+        .isEqualTo(mapOf(17, "sdk17.jar", 19, "sdk19.jar"));
+  }
+
+  @Test
+  public void figureSdksAcceptsIntLists() throws Exception {
+    assertThat(plugin.figureSdks(Arrays.asList(17, 19), defaultSdks))
+        .isEqualTo(mapOf(17, "sdk17.jar", 19, "sdk19.jar"));
+  }
+
+  @Test
+  public void figureSdksAcceptsMapWithCoordinates() throws Exception {
+    Map<Integer, Object> config = mapOf(
+        17, "org.xxx:mysdk:1.2.3",
+        19, "/path/to/another19.jar",
+        20, new File("/path/somewhere.jar")
+        );
+
+    assertThat(plugin.figureSdks(config, defaultSdks))
+        .isEqualTo(mapOf(
+            17, "org.xxx:mysdk:1.2.3",
+            19, new File("/path/to/another19.jar"),
+            20, new File("/path/somewhere.jar")
+        ));
+  }
+
+  @Test
+  public void pluginAddsTaskToDownloadAndroidSdks() {
     Project project = ProjectBuilder.builder()
         .withProjectDir(gradleProjectDir).build();
     project.getPluginManager().apply("org.robolectric");
@@ -37,23 +86,44 @@ public class GradlePluginTest {
         .isInstanceOf(DownloadAndroidSdks.class);
   }
 
-  @Test
-  public void loadingProject() {
-    GradleRunner gradleRunner = GradleRunner.create()
-        .withProjectDir(gradleProjectDir)
-        .withPluginClasspath()
-        .withArguments("--stacktrace", "clean", "test")
-        .withEnvironment(Map.of(
-            "gradle-robolectric-plugin.classpath", System.getenv("gradle-robolectric-plugin.classpath"),
-                "ANDROID_SDK_ROOT", androidSdkRoot
-            ))
-//        .withDebug(true)
-        .forwardOutput();
-    BuildResult buildResult = gradleRunner.buildAndFail();
-    System.out.println("buildResult = " + buildResult.getOutput());
-    System.out.println(buildResult.getTasks());
-    assertThat(buildResult.getOutput()).contains("EverythingWorkedException");
-//        def project = ProjectBuilder.builder().withProjectDir(gradleProjectDir).build()
-//        project.exec()
+  @Ignore("evaluating project doesn't work") @Test
+  public void pluginAddsRobolectricToTestImplementationDependencies() {
+    Project project = ProjectBuilder.builder()
+        .withProjectDir(gradleProjectDir).build();
+    project.getPluginManager().apply("org.robolectric");
+    ((DefaultProject) project).evaluate();
+
+    DependencySet testIntegrationDeps =
+        project.getConfigurations().getByName("testIntegration").getDependencies();
+
+    List<Dependency> deps = new ArrayList<>();
+    testIntegrationDeps.forEach(dependency -> {
+          if (dependency.getGroup().equals("org.robolectric")) {
+            deps.add(dependency);
+          }
+        }
+    );
+    System.out.println("deps = " + deps);
+  }
+
+  static <K, V> Map<K, V> mapOf(K k1, V v1) {
+    HashMap<K, V> map = new HashMap<>();
+    map.put(k1, v1);
+    return map;
+  }
+  
+  static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2) {
+    HashMap<K, V> map = new HashMap<>();
+    map.put(k1, v1);
+    map.put(k2, v2);
+    return map;
+  }
+
+  static <K, V> Map<K, V> mapOf(K k1, V v1, K k2, V v2, K k3, V v3) {
+    HashMap<K, V> map = new HashMap<>();
+    map.put(k1, v1);
+    map.put(k2, v2);
+    map.put(k3, v3);
+    return map;
   }
 }
